faa79cf0f59dfa6341c0e2d612bb10100425be45
Gustavo Lopez <gml.gustavoml@gmail.com>
2020-08-17 14:29:05 -0300

    Improvements for get value from object

--- react__deCeroAExperto/intro-javascript/src/index.js
+++ react__deCeroAExperto/intro-javascript/src/index.js
@@ -3,40 +3,57 @@ import { tasks } from './data/tasks';
 function findObjectByKey(array, year, month, day) {
   let newObject = undefined;
   let value;
+
+  const isValueUndefinded = () => {
+    if (year && value.length === 0) return true;
+    if (month && value[0][year][month] === undefined) return true;
+    if (day && value[0][year][month][day] === undefined) return true;
+    return false;
+  };
+
+  const setYear = () => {
+    if (isValueUndefinded()) return undefined;
+    newObject = {
+      [year]: value[0][year]
+    };
+  };
+
+  const setMonth = () => {
+    if (month) {
+      if (isValueUndefinded()) return undefined;
+      newObject = {
+        [year]: {
+          [month]: value[0][year][month]
+        }
+      };
+    }
+  };
+
+  const setDay = () => {
+    if (day) {
+      if (isValueUndefinded()) return undefined;
+      newObject = {
+        [year]: {
+          [month]: {
+            [day]: value[0][year][month][day]
+          }
+        }
+      };
+    }
+  };
+
   if (array && year) {
     value = array.filter((item) => {
       return Object.keys(item)[0] === year;
     });
 
-    if (value.length > 0) {
-      newObject = {
-        [year]: value[0][year]
-      };
-
-      if (month) {
-        if (value[0][year][month] === undefined) return undefined;
-        newObject = {
-          [year]: {
-            [month]: value[0][year][month]
-          }
-        };
-
-        if (day) {
-          if (value[0][year][month][day] === undefined) return undefined;
-          newObject = {
-            [year]: {
-              [month]: {
-                [day]: value[0][year][month][day]
-              }
-            }
-          };
-        }
-      }
-    }
+    setYear();
+    setMonth();
+    setDay();
   }
 
   return newObject;
 }
 
-const result = findObjectByKey(tasks, '2020', 'JAN');
+const result = findObjectByKey(tasks, '2019', 'JAN', '02');
 console.log(result);
