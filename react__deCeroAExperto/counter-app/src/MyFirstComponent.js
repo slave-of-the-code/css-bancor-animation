c26b7a233058f790fbb4c8899fe5fa9fee8a02e1
Gustavo Lopez <gml.gustavoml@gmail.com>
2020-08-30 20:48:21 -0300

    Creation of my first component

---
+++ react__deCeroAExperto/counter-app/src/MyFirstComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MyfirstComponent.css';
+// const HelloReact = () => {
+//   return <h1>hello react!</h1>;
+// };
+
+const MyFirstComponent = () => {
+  return (
+    <header>
+      <h1>My first component</h1>
+    </header>
+  );
+};
+
+export default MyFirstComponent;
--- react__deCeroAExperto/counter-app/src/index.js
+++ react__deCeroAExperto/counter-app/src/index.js
@@ -2,11 +2,28 @@
 // console.log("i am react!!");
 
 // #2
-import React from 'react'; // este "React" se DEBE llamar asi, respetando case sensitive
-// import ReactDOM from "react-dom"; // en cambio, aqui puede llamarse como deseen
-import myDom from 'react-dom';
+// import React from 'react'; // este "React" se DEBE llamar asi, respetando case sensitive
+// // import ReactDOM from "react-dom"; // en cambio, aqui puede llamarse como deseen
+// import myDom from 'react-dom';
 
-const helloWorld = <h1>Hello REACT world</h1>;
+// const helloWorld = <h1>Hello REACT world</h1>;
+// const divRoot = document.querySelector('#root');
+
+// myDom.render(helloWorld, divRoot);
+
+// #3
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyFirstComponent from './MyFirstComponent';
+
+// Styles
+import './index.css';
+
 const divRoot = document.querySelector('#root');
 
-myDom.render(helloWorld, divRoot);
+ReactDOM.render(
+  <>
+    <MyFirstComponent />
+  </>,
+  divRoot
+);
